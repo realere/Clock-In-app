baef3c142b3d8c782a2081c06a3a0d4d31cf9ade
Alaere Samuel <prettyere@gmail.com>
2018-05-05 16:23:02 +0100

    date and time javascript working now refactoring

---
+++ public_html/clockinjavascript.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+    var date = Date();
+    document.getElementById("displayDateTime").innerHTML = date;
+
+
+
+
+//var date = Date();
+//    document.getElementById("displayDateTime").innerHTML = date;
+//    
+//function myFunction() {
+//    var date = Date();
+//    document.getElementById("displayDateTime").innerHTML = date;
+//}
