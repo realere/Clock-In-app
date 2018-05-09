bfb3b543e230a6b5d08b53095c8f95376ce68598
Alaere Samuel <prettyere@gmail.com>
2018-05-09 10:53:17 +0100

    working on links

---
+++ public_html/clockin.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var date = Date();
+ document.getElementById("displayDateTime").innerHTML = date;
+
+
+//fuction getDate(){
+//    date = Date();
+//    document.getElementById("displayDateTime").innerHTML = date;
+//}
+
+
+function init (){
+    document.addEventListener("deviceready", onDeviceReady, False );
+    storage = window.localStorage;
+}
+
+function  onDeviceReady(){
+    var node = document.createElement('link');
+    node.setAttribute('rel', 'stylesheet');
+    node.setAttribute('type','text' );
+    if (cordova.platformId == 'ios'){
+        node.setAttribute('href', 'clockinios.css');
+        window.StatusBar.overlayWebView(false);
+        window.StatusBar.styleDefault();
+    }else{
+        node.setAttribute('href', 'clockinandroid.css');
+        wind.StatusBar.backgroundColorByHexString('#1565C0');
+    }
+}
