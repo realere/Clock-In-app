b471102b5ea1d4c7f1cdaf30fd2357ffbbf8cfc2
Alaere Samuel <prettyere@gmail.com>
2018-05-10 18:19:54 +0100

    added person to javascript

--- public_html/clockin.js
+++ public_html/clockin.js
@@ -4,9 +4,59 @@
  * and open the template in the editor.
  */
 
-
 var date = Date();
  document.getElementById("displayDateTime").innerHTML = date;
+
+
+var student;
+
+student = {
+    name: "Mark",
+    student_id: "S12345",
+    lecture_date: "today",
+    course: "Human Computer Interaction",
+    lecture_address: "M 517",
+    lecture_time: "30 mintues" 
+    
+};
+
+
+
+
+//Human Computer Interaction <br> 
+//                    Today at <br>
+//                    M 517 <br> 
+//                    in 30 Minutes 
+
+function description(){
+    
+};
+
+document.getElementById("nextClass").innerHTML = 
+        student.course + "  " + student.lecture_date 
+         + " at " + student.lecture_address+ " in " + student.lecture_time;
+
+
+
+//function getInfo() {
+//    var username = document.getElementById("username").value;
+//    var password = document.getElementById("password").value;
+//    
+//    
+//    
+//     for(i = 0; i < objStudent.length; i++)
+//        
+         
+        
+//     {
+//        if(username === objStudent.username && password === objStudent.password) {
+//            console.log(username);
+//        } 
+//    }
+//}
+
+
+
 
 
 
@@ -21,7 +71,7 @@ function  onDeviceReady(){
     var node = document.createElement('link');
     node.setAttribute('rel', 'stylesheet');
     node.setAttribute('type','text' );
-    if (cordova.platformId == 'ios'){
+    if (cordova.platformId === 'ios'){
         node.setAttribute('href', 'clockinios.css');
         window.StatusBar.overlayWebView(false);
         window.StatusBar.styleDefault();
@@ -30,3 +80,4 @@ function  onDeviceReady(){
         wind.StatusBar.backgroundColorByHexString('#1565C0');
     }
 }
+
